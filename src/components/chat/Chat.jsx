229889c54330bfdbc018924a0a8d8020491fdba4
saksham <sakshamshrestha400@gmail.com>
2025-08-15 10:44:04 +0545

    added ui design for chat and detail components

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 function Chat() {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const endRef = useRef(null)
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({behaviour: "smooth"})
+  })
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  console.log(text);
+
   return (
     <div className="chat">
       <div className="top">
@@ -13,13 +30,81 @@ function Chat() {
           </div>
         </div>
         <div className="icons">
-            <img src="./phone.png" alt="" />
-            <img src="./video.png" alt="" />
-            <img src="./info.png" alt="" />
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
-      <div className="bottom"></div>
+      <div className="center">
+        <div className="message">
+           <img src="./avatar.png" alt="" />
+           <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, blanditiis illo? Doloribus sequi harum aperiam officia laudantium ad maxime eius.</p>
+            <span>1 min ago</span>
+           </div>
+        </div>
+        <div className="message own">
+           <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, blanditiis illo? Doloribus sequi harum aperiam officia laudantium ad maxime eius.</p>
+            <span>1 min ago</span>
+           </div>
+        </div>
+        <div className="message">
+           <img src="./avatar.png" alt="" />
+           <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, blanditiis illo? Doloribus sequi harum aperiam officia laudantium ad maxime eius.</p>
+            <span>1 min ago</span>
+           </div>
+        </div>
+        <div className="message own">
+           <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, blanditiis illo? Doloribus sequi harum aperiam officia laudantium ad maxime eius.</p>
+            <span>1 min ago</span>
+           </div>
+        </div>
+        <div className="message">
+           <img src="./avatar.png" alt="" />
+           <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, blanditiis illo? Doloribus sequi harum aperiam officia laudantium ad maxime eius.</p>
+            <span>1 min ago</span>
+           </div>
+        </div>
+         <div className="message own">
+           <div className="texts">
+            <img src="https://images.pexels.com/photos/33231473/pexels-photo-33231473.jpeg" alt="" />
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, blanditiis illo? Doloribus sequi harum aperiam officia laudantium ad maxime eius.</p>
+            <span>1 min ago</span>
+           </div>
+        </div>
+
+        <div ref={endRef}></div>
+       
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </div>
   );
 }
